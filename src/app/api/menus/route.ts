41dad6ca58e134ccfbece13735d9382671a2774c
gouz7514 <gouz7514@gmail.com>
2023-05-30 23:45:40 +0900

    Fix : menu route에서 사용하지 않는 params 제거

--- src/app/api/menus/route.ts
+++ src/app/api/menus/route.ts
@@ -1,8 +1,7 @@
 import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
-import { NextApiRequest, NextApiResponse } from 'next'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   const menus = await prisma.menu.findMany()
   
   return NextResponse.json(menus)
