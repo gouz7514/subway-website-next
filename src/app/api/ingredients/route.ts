a83f123028ba813ac25c1fa436db2f2cf5fd9cb3
gouz7514 <gouz7514@gmail.com>
2023-05-30 23:43:10 +0900

    Fix : NextApiRequest -> NextRequest

--- src/app/api/ingredients/route.ts
+++ src/app/api/ingredients/route.ts
@@ -1,9 +1,9 @@
 import prisma from "@/lib/prisma"
-import { NextResponse } from "next/server"
+import { NextResponse, NextRequest } from "next/server"
 import { NextApiRequest, NextApiResponse } from 'next'
 import { parse } from "url"
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const { query } = parse(req.url!, true)
   const { type } = query
 
